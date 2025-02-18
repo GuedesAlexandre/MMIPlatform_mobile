6d612863b94c2dccc14c70da54ff2618894b0c2a
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2025-02-18 19:33:25 +0100

    fix(): detect os befor get ip

--- update_env.sh
+++ update_env.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-IP_ADDRESS=$(ipconfig getifaddr en0)
 
-sed -i '' "s|http://\[ADRESSE IP DU PC\]:8080|http://$IP_ADDRESS:8080|g"  .env
+OS=$(uname)
+
+if [[ "$OS" == "Darwin" ]]; then
+    IP_ADDRESS=$(ipconfig getifaddr en0)
+elif [[ "$OS" == "Linux" ]]; then
+    IP_ADDRESS=$(hostname -I | awk '{print $1}')
+elif [[ "$OS" == "MINGW64_NT"* || "$OS" == "MSYS_NT"* || "$OS" == "CYGWIN_NT"* ]]; then
+    IP_ADDRESS=$(ipconfig | grep 'IPv4' | awk '{print $NF}' | head -n 1)
+else
+    echo "Système d'exploitation non supporté"
+    exit 1
+fi
+
+sed -i '' "s|http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:8080|http://$IP_ADDRESS:8080|g" .env
 
 echo "L'adresse IP $IP_ADDRESS a été intégrée dans le fichier .env"
