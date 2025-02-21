6d12a6a35aa3e99e84d0c6a8d32cc471f5ccc245
AGuedes <115166939+GuedesAlexandre@users.noreply.github.com>
2025-02-21 11:28:01 +0100

    fix() : sed fixed

--- update_env.sh
+++ update_env.sh
@@ -7,12 +7,16 @@ if [[ "$OS" == "Darwin" ]]; then
 elif [[ "$OS" == "Linux" ]]; then
     IP_ADDRESS=$(hostname -I | awk '{print $1}')
 elif [[ "$OS" == "MINGW64_NT"* || "$OS" == "MSYS_NT"* || "$OS" == "CYGWIN_NT"* ]]; then
-    IP_ADDRESS=$(ipconfig | grep 'IPv4' | awk '{print $NF}' | head -n 1)
+    IP_ADDRESS=$(ipconfig | grep -A 1 'Ethernet adapter' | grep 'IPv4' | awk '{print $NF}')
 else
     echo "Système d'exploitation non supporté"
     exit 1
 fi
 
-sed -i '' "s|http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:8080|http://$IP_ADDRESS:8080|g" .env
+if [[ "$OS" == "Darwin" ]]; then
+    sed -i '' "s|http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:8080|http://$IP_ADDRESS:8080|g" .env
+else
+    sed -i "s|http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:8080|http://$IP_ADDRESS:8080|g" .env
+fi
 
 echo "L'adresse IP $IP_ADDRESS a été intégrée dans le fichier .env"
